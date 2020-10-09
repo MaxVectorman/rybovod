b5deb99ccbdaaf7a209314ac2a9060a7c3a2539e
maximus <vectorman777@gmail.com>
2020-10-09 21:12:24 +0300

    start

---
+++ js/ORIGINscript.js
@@ -0,0 +1,11 @@
+/*
+document.createElement('header' );
+document.createElement('footer' );
+document.createElement('section');
+document.createElement('aside'  );
+document.createElement('nav'    );
+document.createElement('article');
+document.createElement('time'   );
+*/
+
+
---
+++ js/script.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function() {
+
+
+/*
+Switch #1
+(function($) {
+$('button').on('click',function(e) {
+if ($(this).hasClass('grid')) {
+$('#container ul').removeClass('list').addClass('grid');
+}
+else if($(this).hasClass('list')) {
+$('#container ul').removeClass('grid').addClass('list');
+}
+});
+})(jQuery);
+*/
+
+
+
+
+}); //Конец ready
+
+
+
+
